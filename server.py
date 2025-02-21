9e781f82090269394c018f1c7652e735a835318a
AshishRam7 <jaashishram7@gmail.com>
2025-02-21 18:53:14 +0530

    fixed small issue

--- server.py
+++ server.py
@@ -56,7 +56,9 @@ APP_SECRET = os.getenv("APP_SECRET", "e18fff02092b87e138b6528ccfa4a1ce")
 VERIFY_TOKEN = os.getenv("VERIFY_TOKEN", "fitvideodemo")
 access_token = "IGAAI8SJHk0mNBZAFB6TF9zejQtcnoyWWlOaGRSaEJyRGlfTXVUMEdveGJiVURXRXNlOUUwZA0QwQ2w4ZAi1HVE5mM2tqdk1jYW94VHVQbHdnWUx1NVduTHg1QzRMY1BzMVdqaEpId3B3X0JxNzM4dWJmWGtsWnZAKb1p4SnNiRzFMZAwZDZD"  # Replace with your actual token
 account_id = "17841472117168408"  # Replace
-gemini_api_key = os.getenv("GEMINI_API_KEY","AIzaSyDgH-W60Vk--3rSbTq91lzYoMfc1j1RzFE") # Use environment variable, replace default
+gemini_api_key = os.getenv("GEMINI_API_KEY","AIzaSyDgH-W60Vk--3rSbTq91lzYoMfc1j1RzFE")
+ # Use environment variable, replace default
+gemini_api_key = "AIzaSyDgH-W60Vk--3rSbTq91lzYoMfc1j1RzFE"
 model_name = "gemini-1.5-flash"
 
 default_dm_response_positive = "Thanks for your kind words! We appreciate your support."
